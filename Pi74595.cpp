04aceb46858542e7ae851e2bdb8e778c6e9b573d
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-08-10 20:34:29 +0800

    no message

--- Pi74595.cpp
+++ Pi74595.cpp
@@ -105,29 +105,29 @@ bool CheckKey(int key){
 			
 			// register是從最後一顆開始往回存，7->0
 			for( int j = 7; j >= 0; j--){
-				usleep(100);
+				//usleep(100);
 				bcm2835_gpio_write(DI_PIN, j == highBit ? HIGH : LOW);
-				usleep(100);
+				//usleep(100);
 				bcm2835_gpio_write(CL_PIN, LOW);
-				usleep(100);
+				//usleep(100);
 				bcm2835_gpio_write(CL_PIN, HIGH);
 			}
 		}
 		else{
 			for( int j = 0; j < 8; j++){
-				usleep(100);
+				//usleep(100);
 				bcm2835_gpio_write(CL_PIN, LOW);
-				usleep(100);
+				//usleep(100);
 				bcm2835_gpio_write(DI_PIN, LOW);
-				usleep(100);
+				//usleep(100);
 				bcm2835_gpio_write(CL_PIN, HIGH);
 			}
 		}
 	}
-	usleep(100);
+	//usleep(100);
 	bcm2835_gpio_write(CE_PIN, HIGH);
 	
-	usleep(5000);
+	usleep(500);
 	if(bcm2835_gpio_lev(INPUT_PIN) == HIGH)
 		return true;
 	
