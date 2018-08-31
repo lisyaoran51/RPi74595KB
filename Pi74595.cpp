f530abb2a4808fb9e2a351353eb650d6c4aaa979
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-08-31 14:00:29 +0800

    no message

--- Pi74595.cpp
+++ Pi74595.cpp
@@ -176,7 +176,7 @@ void Play(int key){
 	
 	int pitch = key + 24;
 	
-	string s = string("aplay Audio/German_Concert_D_0") + to_string(pitch+21-9) + string("_083.wav");
+	string s = string("mpg321 Audio/German_Concert_D_0") + to_string(pitch+21-9) + string("_083.mp3");
 	
 	thread t(AplayString, s, key);
 	
