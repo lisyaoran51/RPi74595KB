76ee170777cb9ae4c8785128425a51b8ee34aa95
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-09-05 02:36:42 +0800

    no message

--- paplay_8c.c
+++ paplay_8c.c
@@ -469,9 +469,9 @@ int my_pa_mainloop_run(pa_mainloop *m, int *retval) {
 	int firstRun = 0;
 	
     while ((r = pa_mainloop_iterate(m, 1, retval)) >= 0){
-		if(firstRun < 2){
+		if(firstRun < 10){
 			firstRun++;
-		} else if(firstRun == 2){
+		} else if(firstRun == 10){
 			firstRun++;
 			for(int i = 0; i < 5000; i++){
 				printf("-");
