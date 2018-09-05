9cae2e25d8ded7f5db318d78b8a08e44782efb1e
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-09-05 16:48:50 +0800

    no message

--- paplay_8c.c
+++ paplay_8c.c
@@ -239,6 +239,36 @@ enum {
 
 int main(int argc, char *argv[]){
 	
+	char buffer[BUFFER + 1];  
+    int fd[2];  
+    if (argc != 2) {  
+        fprintf(stderr, "Usage：%s string\n\a", argv[0]);  
+        exit(1);  
+    }  
+    if (pipe(fd) != 0) {  
+        fprintf(stderr, "Pipe Error：%s\n\a", strerror(errno));  
+        exit(1);  
+    }  
+	
+	printf("fd[0]:%d , fd[1]:%d\n", fd[0], fd[1]);
+	
+    if (fork() == 0) {  
+        close(fd[0]);  
+        printf("Child[%d] Write to pipe\n\a", getpid());  
+        snprintf(buffer, BUFFER, "%s", argv[1]);  
+        write(fd[1], buffer, strlen(buffer));  
+        printf("Child[%d] Quit\n\a", getpid());  
+        exit(0);  
+    } else {  
+        close(fd[1]);  
+        printf("Parent[%d] Read from pipe\n\a", getpid());  
+        memset(buffer, '\0', BUFFER + 1);  
+        read(fd[0], buffer, BUFFER);  
+        printf("Parent[%d] Read：%s\n", getpid(), buffer);  
+        exit(1);  
+    }  
+	
+	
 	pa_mainloop* m = NULL;
     int ret = 1, r, c;
     char *bn, *server = NULL;
@@ -409,8 +439,7 @@ int main(int argc, char *argv[]){
 				"My pid :%d, parent pid:%d\n",
 				getpid(), getppid());
 		int r;
-		char buffer[20];
-		memset(buffer, '\0', 20); 
+		char buffer[20]; 
 		int bytes;
 		
 		printf("start receiving...\n");
