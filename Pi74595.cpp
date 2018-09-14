2c70e59b21b8df33f4f416453dff9df656a5a9b2
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-09-14 18:06:04 +0800

    no message

--- Pi74595.cpp
+++ Pi74595.cpp
@@ -141,7 +141,7 @@ int main(int argc, char **argv) {
 		for(int i = 0; i < 48; i++){
 			if(CheckKey(i)){
 				if(!keyPlaying[i]){
-					//printf("played! %d\n", i);
+					printf("played! %d\n", i);
 					PlayWithThread(i);
 				}
 				keyPlaying[i] = true;
