c1a33fb6da91661029aff5abca20c2374a08b2b4
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-09-17 01:14:43 +0800

    no message

--- Pi74595.cpp
+++ Pi74595.cpp
@@ -318,7 +318,7 @@ int SetAlsa(int flag){
 			while(keyStartSet->ForkFlag  != flag 					|| 
 				  keyStartSet->QueueHead == keyStartSet->QueueTail  || 
 				  keyStartSet->QueueLock)
-				snd_pcm_writei(handle, silence, SILENCE_LENGTH * sizeof(short));
+				snd_pcm_writei(handle, silence, SILENCE_LENGTH);
 			printf("receive play at %d\n", flag);
 			
 			memcpy(wavData, keyStartSet->WavData[keyStartSet->QueueHead], sizeof(wavData));
